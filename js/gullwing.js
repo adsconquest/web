851844177906121ede1670fd307674a79b21a57f
CMN17203 <CMN17203@CMN17203.local>
2017-04-17 14:18:34 +0900

    over update

--- js/gullwing.js
+++ js/gullwing.js
@@ -9,10 +9,6 @@ $(function () {
     setTimeout('rect()'); 
     setTimeout('concierge()'); 
     setTimeout('pig()'); 
-    setTimeout('pig2()', 2000); 
-    setTimeout('pig3()', 4000); 
-    setTimeout('pig4()', 6000); 
-    setTimeout('pig5()', 8000); 
     setTimeout('cherrySmile()');
     setTimeout('smile()', 2000);
 });
@@ -61,43 +57,7 @@ function pig(){
     }, 2500).animate({
         left: '-30%'
     }, 0);
-    setTimeout('pig()', 10000);
-}
-
-function pig2(){
-    $('.horizon2').animate({
-        left: '100%'
-    }, 2500).animate({
-        left: '-30%'
-    }, 0);
-    setTimeout('pig2()', 10000);
-}
-
-function pig3(){
-    $('.horizon3').animate({
-        left: '100%'
-    }, 2500).animate({
-        left: '-30%'
-    }, 0);
-    setTimeout('pig3()', 10000);
-}
-
-function pig4(){
-    $('.horizon4').animate({
-        left: '100%'
-    }, 2500).animate({
-        left: '-30%'
-    }, 0);
-    setTimeout('pig4()', 10000);
-}
-
-function pig5(){
-    $('.horizon5').animate({
-        left: '100%'
-    }, 2500).animate({
-        left: '-30%'
-    }, 0);
-    setTimeout('pig5()', 10000);
+    setTimeout('pig()', 2500);
 }
 
 function cherrySmile(){
